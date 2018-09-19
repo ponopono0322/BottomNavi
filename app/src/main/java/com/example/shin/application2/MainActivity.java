07bc124245f91fa32829c894bfcdb9eeb5fafe8d
dwshi <ponopono0322@gmail.com>
2018-09-19 14:37:52 +0900

    delete title bar

--- app/src/main/java/com/example/shin/application2/MainActivity.java
+++ app/src/main/java/com/example/shin/application2/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.shin.application2;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         Button button1 = findViewById(R.id.button);
--- app/src/main/java/com/example/shin/application2/SubActivity.java
+++ app/src/main/java/com/example/shin/application2/SubActivity.java
@@ -7,14 +7,16 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 
-public class SubActivity extends AppCompatActivity {
+public class SubActivity extends FragmentActivity {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -46,6 +48,7 @@ public class SubActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_sub);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
